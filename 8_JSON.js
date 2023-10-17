ecfaaa6f6f9d764a2bc4f3195d93b763e0295e88
Adnan056 <adnansohail1@yahoo.com>
2023-10-17 16:44:53 +0530

    v1

---
+++ 8_JSON.js
@@ -0,0 +1,40 @@
+//JSON --> Java Script Object Notation
+
+//Object
+// obj1= {
+//     FirstName: "Adnan",
+//     LastName:"Sohail",
+//     UserEmail:"Adnansohail1@yahoo.com",
+//     ContactNo:9876543210,
+// }
+// console.log(obj1)
+// console.log(typeof(obj1))
+
+//JSON
+// obj2={
+//     "FirstName": "Adnan",
+//     "LastName":"Sohail",
+//     "UserEmail":"Adnansohail1@yahoo.com",
+//     "ContactNo":9876543210,
+// }
+// console.log(obj2)
+// console.log(typeof(obj2))
+
+// var objJSON=JSON.stringify(obj1)
+// console.log(objJSON)
+// console.log(typeof(objJSON))//string
+// var objJSON=JSON.parse(objJSON)
+// console.log(objJSON)
+// console.log(typeof(objJSON))//object
+
+//Replacing the data (replace(old,new))
+// var r=objJSON.replace("Adnan","Sameeth")
+// var r=r.replace("Sohail","Farhaan")
+// console.log(r)
+
+//For in loop
+// l1=[10,20,30,40,50]
+// console.log(l1)
+// for(i in l1){
+//     console.log(l1[i])
+// }
